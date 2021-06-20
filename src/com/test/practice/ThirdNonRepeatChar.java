27e3f3a93cdb4d4c08f1bb071c261cb87db41953
Arman <erarman16@gmail.com>
2021-06-21 00:57:13 +0530

    2nd method added to calcualte third non repeat char, better time complexity

--- src/com/test/practice/ThirdNonRepeatChar.java
+++ src/com/test/practice/ThirdNonRepeatChar.java
@@ -1,7 +1,10 @@
 package com.test.practice;
 
+import java.util.*;
+
 public class ThirdNonRepeatChar {
 
+    //Method 1: nested loops
     public static void main(String[] args) {
         String str = "hherrrruvwwvqa";
         char [] ch = str.toCharArray();
@@ -31,5 +34,49 @@ public class ThirdNonRepeatChar {
         }
         if(!absentFlag)
             System.out.println("No third repeated char exists");
+    }
+
+    //ALTER Method ---- using one loop
+    //mapping char and freq in one iteration and
+    //putting in LinkedHashMap
+    //Iterate linked hash map to get 3rd char with freq=1
+    static
+    {
+        //String str = "hherrrruvwwvqa";
+        String str="12ab";
+        //check for empty/null str can be placed
+        char [] ch = str.toCharArray();
+
+        Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
+        //assuming array is not empty or put following code inside previous check
+        map.put(ch[0],1);
+
+        for (int i=1; i<ch.length; i++)
+        {
+            if(null!=map.get(ch[i]))
+            {
+                int val = map.get(ch[i]);
+                map.put(ch[i],++val);
+            }
+            else
+            {
+                map.put(ch[i],1);
+            }
+        }
+
+        Set<Character> keys = map.keySet();
+        int counter=0;
+        for (Character key : keys)
+        {
+            if(map.get(key)==1)
+                counter++;
+            if(counter==3)
+            {
+                System.out.println("Third non rep char: "+key);
+                break;
+            }
+        }
+        if(counter!=3)
+            System.out.println("No third repeat char present");
     }
 }
