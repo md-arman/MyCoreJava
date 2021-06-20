8bead91779ed2e1fc8939756434df324040e8ba4
Arman <erarman16@gmail.com>
2021-06-20 23:40:06 +0530

    practice solution for third non-repeated char in a string

---
+++ src/com/test/practice/ThirdNonRepeatChar.java
@@ -0,0 +1,31 @@
+package com.test.practice;
+
+public class ThirdNonRepeatChar {
+
+    public static void main(String[] args) {
+        String str = "hherrrruvwwvqa";
+        char [] ch = str.toCharArray();
+
+        int counter = 0;
+        //O(n^2)
+        for(int i=0; i<ch.length; i++)
+        {
+            char c1 = ch[i];
+            boolean flag= false;
+            for(int j=0; j<ch.length; j++)
+            {
+                if(i!=j && c1==ch[j]){
+                    flag=true;
+                    break;
+                }
+            }
+            if(!flag)
+                counter++;
+            if(counter==3)
+            {
+                System.out.println(c1);
+                break;
+            }
+        }
+    }
+}
